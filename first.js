7dccce70f10e6ab8cd80690b4d3d2dcfea75a0db
oljisfcy <oljisfcy@163.com>
2022-12-17 23:57:41 +0800

    111

---
+++ first.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.bilibili.com/video/BV1Da411Z7s7/?vd_source=b7d4d9da4fc8d03ef5dcfadd5adc1e46
+// @license      AGPL
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
